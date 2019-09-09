e95d9d178cb5c0d3dfb0468c04d5e22d9564c9f7
Maria Marrero <maria.marrero13@upr.edu>
2019-09-08 22:19:55 -0400

    Complimetary task complete

--- empty.c
+++ empty.c
@@ -17,42 +17,41 @@
 
 #define F_CPU 16000000UL
 
-/*********************** Modify *********/
-// LCD module information
-#define lcd_LineOne     0x00                    // start of line 1
-#define lcd_LineTwo     0x40                    // start of line 2
-
-
 // LCD instructions
 #define Clear           0b00000001
-#define lcd_Home        0b00000010
 #define lcd_EntryMode   0b00000110
 #define Display_off     0b00001011
 #define Display_on      0b00001111
 #define FunctionSet     0b00111000
-#define lcd_SetCursor   0b10000000
+#define lcd_SetCursor       0b10000000          // set cursor position
+#define lcd_LineOne       0x00                  // start of line 1
+#define lcd_LineTwo       0x40                  // start of line 2
+#define lcd_Home            0b00000010          // return cursor to first position on first line
 
 // Function Prototypes
+void leds(void);
+void pushbuttons(void);
+void lcd_basic(void);
+void circular_list(void);
+uint8_t down_pressed(uint8_t, uint8_t);
+uint8_t up_pressed(uint8_t, uint8_t);
 void lcd_byte(uint8_t);
 void lcd_command(uint8_t);
 void lcd_character(uint8_t);
 void lcd_string(uint8_t *);
 void lcd_init(void);
 
-/****************************Modify ***********/
 /*
  *  ======== mainThread ========
  */
 void *mainThread(void *arg0)
 {
-    uint8_t text[]   = "Hello World";
-    uint8_t newtext[]   = "This is Maria";
     /* Call driver init functions */
     GPIO_init();
 
     /* Configure LED pin and PB1 */
-    GPIO_setConfig(Board_GPIO_07, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW);
-    GPIO_setConfig(Board_GPIO_06, GPIO_CFG_IN_PU | GPIO_CFG_IN_INT_FALLING);
+//    GPIO_setConfig(Board_GPIO_07, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW); //LED
+//    GPIO_setConfig(Board_GPIO_06, GPIO_CFG_IN_PU | GPIO_CFG_IN_INT_FALLING); // PUSHBUTTON
     GPIO_setConfig(Board_GPIO_28, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW); //D7
     GPIO_setConfig(Board_GPIO_17, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW); //D6
     GPIO_setConfig(Board_GPIO_30, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW); //D5
@@ -64,33 +63,12 @@ void *mainThread(void *arg0)
     GPIO_setConfig(Board_GPIO_08, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW); //RS
     GPIO_setConfig(Board_GPIO_09, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW); //E
 
-   GPIO_write(Board_GPIO_28, GPIO_OFF); //D7
-   GPIO_write(Board_GPIO_17, GPIO_OFF); //D6
-   GPIO_write(Board_GPIO_30, GPIO_OFF); //D5
-   GPIO_write(Board_GPIO_16, GPIO_OFF); //D4
-   GPIO_write(Board_GPIO_15, GPIO_OFF); //D3
-   GPIO_write(Board_GPIO_25, GPIO_OFF); //D2
-   GPIO_write(Board_GPIO_00, GPIO_OFF); //D1
-   GPIO_write(Board_GPIO_22, GPIO_OFF); //D0
-   GPIO_write(Board_GPIO_08, GPIO_OFF); //RS
-   GPIO_write(Board_GPIO_09, GPIO_OFF); //E
 
-//    /* Turn on user LED */
-//   GPIO_write(Board_GPIO_07, Board_GPIO_LED_ON);
-//
-//    while (1) {
-//        if(GPIO_read(Board_GPIO_06) == 0)
-//            GPIO_write(Board_GPIO_07,Board_GPIO_LED_ON);
-//        else
-//            GPIO_write(Board_GPIO_07,Board_GPIO_LED_OFF);
-//    }
+//    leds();
+//    pushbuttons();
+//    lcd_basic();
+    circular_list();
 
-   lcd_init();
-   lcd_string(newtext);
-   usleep(50);
-   lcd_command(lcd_SetCursor|lcd_LineTwo);
-   usleep(50);
-   lcd_string(text);
 
    while (1) {}
 
@@ -129,50 +107,138 @@ void lcd_init(void)
 }
 
 
-void lcd_string(uint8_t theString[])
+void lcd_string(uint8_t word[])
 {
     volatile int i = 0;
-    while (theString[i] != 0)
+    while (word[i] != 0)
     {
-        lcd_character(theString[i]);
+        lcd_character(word[i]);
         i++;
-        sleep(.1);
+        usleep(50);
     }
 }
 
-void lcd_character(uint8_t theData)
+void lcd_character(uint8_t character)
 {
     GPIO_write(Board_GPIO_08, GPIO_ON);               // Data Register
 
-    lcd_byte(theData);                                // write the data
+    lcd_byte(character);                                // write the data
 }
 
-void lcd_command(uint8_t theInstruction)
+void lcd_command(uint8_t command)
 {
     GPIO_write(Board_GPIO_08, GPIO_OFF);              // Instruction Register
 
-    lcd_byte(theInstruction);                         // write the instruction
+    lcd_byte(command);                         // write the instruction
 }
 
-void lcd_byte(uint8_t theByte)
+void lcd_byte(uint8_t byte)
 {
 
-    GPIO_write(Board_GPIO_28,(unsigned int)(1 & (theByte >> 7 )));
-    GPIO_write(Board_GPIO_17,(unsigned int)(1 & (theByte >> 6 )));
-    GPIO_write(Board_GPIO_30,(unsigned int)(1 & (theByte >> 5 )));
-    GPIO_write(Board_GPIO_16,(unsigned int)(1 & (theByte >> 4 )));
-    GPIO_write(Board_GPIO_15,(unsigned int)(1 & (theByte >> 3 )));
-    GPIO_write(Board_GPIO_25,(unsigned int)(1 & (theByte >> 2 )));
-    GPIO_write(Board_GPIO_00,(unsigned int)(1 & (theByte >> 1 )));
-    GPIO_write(Board_GPIO_22,(unsigned int)(1 & (theByte >> 0 )));
+    GPIO_write(Board_GPIO_28,(unsigned int)(1 & (byte >> 7 )));
+    GPIO_write(Board_GPIO_17,(unsigned int)(1 & (byte >> 6 )));
+    GPIO_write(Board_GPIO_30,(unsigned int)(1 & (byte >> 5 )));
+    GPIO_write(Board_GPIO_16,(unsigned int)(1 & (byte >> 4 )));
+    GPIO_write(Board_GPIO_15,(unsigned int)(1 & (byte >> 3 )));
+    GPIO_write(Board_GPIO_25,(unsigned int)(1 & (byte >> 2 )));
+    GPIO_write(Board_GPIO_00,(unsigned int)(1 & (byte >> 1 )));
+    GPIO_write(Board_GPIO_22,(unsigned int)(1 & (byte >> 0 )));
 
 
 // write the data
-    usleep(50);
+    usleep(.50);
     GPIO_write(Board_GPIO_09,GPIO_ON);
-    usleep(50);
+    usleep(.50);
     GPIO_write(Board_GPIO_09,GPIO_OFF);
-    usleep (50);
-   //printf("The byte = %d %d %d %d %d %d %d %d \n", (unsigned int) 1 & (theByte >> 7 ), (unsigned int)1 & (theByte >> 6 ), (unsigned int) 1 & (theByte >> 5),  1 & (theByte >> 4 ), 1 & (theByte >> 3 ),  1 & (theByte >> 2 ),  1 & (theByte >> 1),  1 & (theByte >> 0 ));
+    usleep (.50);
+
 }
+void leds(void){
+    while(1){
+        GPIO_toggle(Board_GPIO_07);
+        sleep(1);
+    }
+}
+void pushbuttons(void){
+
+        while (1) {
+            if(GPIO_read(Board_GPIO_06) == 0)
+                GPIO_write(Board_GPIO_07,Board_GPIO_LED_ON);
+            else
+                GPIO_write(Board_GPIO_07,Board_GPIO_LED_OFF);
+        }
+}
+void lcd_basic(void){
+    uint8_t text[] = "Hello World";
+    uint8_t text2[] = "How u doin";
+
+    lcd_init();
+    lcd_string(text);
+    usleep(50);
+    lcd_command(lcd_Home);
+    lcd_command(lcd_SetCursor|lcd_LineTwo);
+    usleep(50);
+    lcd_string(text2);
+}
+void circular_list(void){
+    const int STRINGS = 16;
+      uint8_t text[STRINGS][16] = {"Maria","Alejandra","Marrero", "Ortiz",
+                                   "Yamil", "Jose", "Gonzalez", "Zuaznabar",
+                                   "Diego", "Jose","Amador", "Bonilla",
+                                   "Christian", "Antonio", "Santiago", "Berio"};
+      uint8_t positions[] = {-1,0};
+      GPIO_setConfig(Board_GPIO_07, GPIO_CFG_IN_PU | GPIO_CFG_IN_INT_FALLING); //LED
+      GPIO_setConfig(Board_GPIO_06, GPIO_CFG_IN_PU | GPIO_CFG_IN_INT_FALLING); // PUSHBUTTON
+while(1){
+//    //if pushdown
+      if(GPIO_read(Board_GPIO_07) == 1){          //button down pressed --CHANGE GPIO
+          lcd_init();
+          lcd_command(lcd_Home);
+          usleep(50);
+          int oldpos = positions[1];
+          positions[0] = positions[1];
+          positions[1] = down_pressed(oldpos,STRINGS);
+          puts(text[positions[0]]);
+          puts(text[positions[1]]);
+          lcd_string(text[positions[0]]);
+          usleep(50);
+          lcd_command(lcd_Home);
+          lcd_command(lcd_SetCursor|lcd_LineTwo);
+          usleep(50);
+          lcd_string(text[positions[1]]);
+
+      }
+      if(GPIO_read(Board_GPIO_06) == 0){          //button up pressed -CHANGE GPIO
+          lcd_init();
+          lcd_command(lcd_Home);
+          usleep(50);
+          int oldpos = positions[0];
+          positions[0] = up_pressed(oldpos,STRINGS);
+          positions[1] = oldpos;
+          puts(text[positions[0]]);
+          puts(text[positions[1]]);
+          lcd_string(text[positions[0]]);
+          usleep(50);
+          lcd_command(lcd_Home);
+          lcd_command(lcd_SetCursor|lcd_LineTwo);
+          usleep(50);
+          lcd_string(text[positions[1]]);
+      }
+
+}
+}
+uint8_t down_pressed(uint8_t index,uint8_t size){
+        if(index == ( size -1))
+            return 0;
+        else
+            return index +1;
+    }
+
+uint8_t up_pressed(uint8_t index, uint8_t size){
+        if(index == 0)
+            return size-1;
+        else
+            return index -1;
+    }
+
 
