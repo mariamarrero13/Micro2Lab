51650480774fb166b2445b4db8a0fc60f8216edb
Maria Marrero <maria.marrero13@upr.edu>
2019-10-31 16:21:18 -0400

    LAST LAB FINISHEEEEEEEEEEEEEEEEEEEDDDDDDDD

--- Lab7.c
+++ Lab7.c
@@ -11,6 +11,7 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 #include <ti/drivers/GPIO.h>
 #include <ti/drivers/Timer.h>
 #include <ti/drivers/PWM.h>
@@ -26,6 +27,7 @@ uint8_t o_index = 13;
 uint8_t index = 0;
 uint8_t direction =  1 ;
 uint_fast16_t adcValue;
+uint8_t status = 0;
 
 
 
@@ -44,23 +46,17 @@ void PWMod_init();
  * ISRs
  */
 
-void change_dac_value() //Timer_Handle myHandle
+void change_dac_value(Timer_Handle myHandle) //
 {
-    if(++index >12) index =0;
+    //comment for sin wave
+    if(++index ==12) index =0;
 
-    //sin wave
-    //    if(direction == 1){
-    //        if(++index > 12){
-    //            index = 11;
-    //            direction = 2; //decreasing
-    //        }
-    //    }
-    //    else{
-    //        if(--index < 0x00){
-    //            index++;
-    //            direction = 1; //increasing
-    //        }
-    //    }
+    //uncomment for sin wave
+    //        if(++index ==50) index =0;
+}
+void change_units(uint_least8_t index){
+    if(status == 1) status =0;
+    else status = 1;
 }
 
 
@@ -69,8 +65,8 @@ void *mainThread(void *arg0)
 
     //    generating_volts();
     //    reading_volts();
-    dimmer();
-    //complimentary_task();
+    //    dimmer();
+    complimentary_task();
 
 
     return (NULL);
@@ -93,27 +89,31 @@ void generating_volts()
 
     //comment this for sin wave
     uint8_t dac_values[] = {0x00, 0x17, 0x2E, 0x45, 0x5C, 0x73, 0x8A, 0xA1, 0xB8, 0xCF, 0xE6, 0xFF};
-
-    //    init_timer(1000000,Timer_CONTINUOUS_CALLBACK,Timer_PERIOD_US, change_dac_value);
+    init_timer(1000000,Timer_CONTINUOUS_CALLBACK,Timer_PERIOD_US, change_dac_value);
     while(1){
         if(o_index!=index){
             write_to_port(dac_values[index]);
             o_index = index;
-            printf("%d\n", index);
         }
-        sleep(1);
-        change_dac_value();
 
     }
     //comment this for sin wave
 
-    //sin wave
-    //to 500Hz, period == 2000us
-    //remember to uncomment code in ISR
-    //        init_timer(2000,Timer_CONTINUOUS_CALLBACK,Timer_PERIOD_US, change_dac_value);
+    /*sin wave
+     *to 500Hz, period == 2000us
+    remember to uncomment code in ISR*/
+
+    //    uint8_t dac_values[] = {0x80,0x8f,0x9f,0xae,0xbd,0xca,0xd7,0xe2,
+    //                            0xeb,0xf3,0xf9,0xfd,0xff,0xff,0xfd,0xf9,
+    //                            0xf3,0xeb,0xe2,0xd7,0xca,0xbd,0xae,0x9f,
+    //                            0x8f,0x80,0x70,0x60,0x51,0x42,0x35,0x28,
+    //                            0x1d,0x14,0xc,0x6,0x2,0x0,0x0,0x2,
+    //                            0x6,0xc,0x14,0x1d,0x28,0x35,0x42,0x51,
+    //                            0x60,0x70,0x80};
+    //    init_timer(40,Timer_CONTINUOUS_CALLBACK,Timer_PERIOD_US, change_dac_value);
     //    while(1){
     //        if(o_index!=index){
-    //            write_to_port(index);
+    //            write_to_port(dac_values[index]);
     //            o_index = index;
     //        }
     //    }
@@ -127,6 +127,7 @@ void reading_volts()
     uint8_t sec[1];
     uint8_t third[1];
     uint8_t fourth[1];
+    uint8_t hex[2];
 
     ADC_init();
     lcd_init_4bit(Board_GPIO_28,Board_GPIO_17,Board_GPIO_16,Board_GPIO_15,Board_GPIO_22,Board_GPIO_25);
@@ -141,6 +142,11 @@ void reading_volts()
         if (result == ADC_STATUS_SUCCESS)
         {
             adcValueUv = ADC_convertToMicroVolts(adcHandle,adcValue)/421,530;
+            lcd_command(lcd_SetCursor|lcd_LineOne);
+            sprintf(hex, "%02x", adcValue);
+            lcd_string(hex);
+
+            lcd_command(lcd_SetCursor|lcd_LineTwo);
             ltoa( adcValueUv%10,fourth);
             adcValueUv/=10;
             ltoa( adcValueUv%10,third);
@@ -155,7 +161,7 @@ void reading_volts()
             lcd_string(sec);
             lcd_string(third);
             lcd_string(fourth);
-            lcd_command(lcd_SetCursor|lcd_LineOne);
+
             sleep(1);
 
         }
@@ -203,6 +209,56 @@ void dimmer()
         }
         usleep(1000);
     }
+}
+void complimentary_task()
+{
+    ADC_init();
+    GPIO_init();
+
+    GPIO_setConfig(Board_GPIO_BUTTON1, GPIO_CFG_IN_PU | GPIO_CFG_IN_INT_FALLING);  // pin 2 used, pb_out1 in lcd
+    GPIO_setCallback(Board_GPIO_BUTTON1, change_units);
+    GPIO_enableInt(Board_GPIO_BUTTON1);
+    lcd_init_4bit(Board_GPIO_28,Board_GPIO_17,Board_GPIO_16,Board_GPIO_15,Board_GPIO_22,Board_GPIO_25);
+    lcd_init_4bit(Board_GPIO_28,Board_GPIO_17,Board_GPIO_16,Board_GPIO_15,Board_GPIO_22,Board_GPIO_25);
+    uint8_t result;
+    uint32_t adcValueUv;
+    uint32_t tempF;
+    ADC_Params params;
+    uint8_t lcd_temp[1];
+    uint8_t decimal[1];
+    ADC_Params_init(&params);
+    ADC_Handle adcHandle = ADC_open(CC3220S_LAUNCHXL_ADC0, &params);  //pin 59 used
+    while(1)
+    {
+        result =  ADC_convert(adcHandle, &adcValue);
+        if (result == ADC_STATUS_SUCCESS)
+        {
+            adcValueUv = ADC_convertToMicroVolts(adcHandle,adcValue)/421,530;
+            lcd_command(lcd_SetCursor|lcd_LineOne);
+            //write value to lcd
+            if(status == 0){
+                sprintf(lcd_temp, "%02d", adcValueUv/10);
+                lcd_string("Temp in C: ");
+                lcd_string(lcd_temp);
+                ltoa(adcValueUv%10,decimal);
+                lcd_string(".");
+                lcd_string(decimal);
+            }
+            else{
+                tempF = ((adcValueUv * 9)/5) + 320 ;
+                sprintf(lcd_temp, "%02d", tempF/10);
+                lcd_string("Temp in F: ");
+                lcd_string(lcd_temp);
+                ltoa(adcValueUv%10,decimal);
+                lcd_string(".");
+                lcd_string(decimal);
+            }
+
+
+        }
+    }
+
+
 }
 
 
